dd8ca7c1104f30604655cc91b51d21c8c5dc48fc
ekosulistyoa <eko.sulistyoa@gmail.com>
2025-02-18 11:43:13 +0700

    third

--- src/stienway.java
+++ src/stienway.java
@@ -2,5 +2,8 @@ public class stienway {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("another commit");
+
+        System.out.println("--------------------------------------------");
+        System.out.println("now i understand the commit in the 3rd try");
     }
 }
