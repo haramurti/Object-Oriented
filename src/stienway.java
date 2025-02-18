d7c59e429fbb330a7d596c1b8b83d45579eadb15
ekosulistyoa <eko.sulistyoa@gmail.com>
2025-02-18 11:40:31 +0700

    second

--- src/stienway.java
+++ src/stienway.java
@@ -1,5 +1,6 @@
 public class stienway {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("another commit");
     }
 }
