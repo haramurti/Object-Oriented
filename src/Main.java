952cddc4ce4ae969e51cfdb5354f931ddc344f2d
ekosulistyoa <eko.sulistyoa@gmail.com>
2025-02-24 15:29:31 +0700

    Git remote push

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,7 @@ public class Main  {
         System.out.println(stienwayin.y);
 
         //Abstarction trough inheritißnce
+        System.out.println("  ");
         s.showBahanBenang();
     }
 }
